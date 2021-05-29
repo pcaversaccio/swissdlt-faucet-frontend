35c8b4ec11ec29951957f0e1362f9429f3c4b821
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2021-05-29 12:49:37 +0200

    feat: contract interaction now works - fixed by enhancing polyfills.ts, fuck you webpack v5

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -66,4 +66,9 @@ import 'zone.js';  // Included with Angular CLI.
  (window as any).global = window;
  import { Buffer } from 'buffer';
  global.Buffer = Buffer;
+ global.process = {
+    env: { DEBUG: undefined },
+    version: '',
+    nextTick: require('next-tick')
+    } as any;
  
